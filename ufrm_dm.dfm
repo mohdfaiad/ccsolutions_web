34b9b5faafc13ba575f020444c0826d47d5fc4d9
Elton Bahia <eltonbahiabc@gmail.com>
2018-07-11 15:39:55 -0400

    #atualizacao

--- ufrm_dm.dfm
+++ ufrm_dm.dfm
@@ -1,5 +1,31 @@
-object dm: Tdm
+object frm_dm: Tfrm_dm
   OldCreateOrder = False
-  Height = 234
-  Width = 221
+  Height = 412
+  Width = 541
+  object conn_ccs: TFDConnection
+    Params.Strings = (
+      'Database=ccs'
+      'User_Name=sa'
+      'Password=ccspassword'
+      'Server=192.168.88.150'
+      'DriverID=MySQL')
+    Connected = True
+    LoginPrompt = False
+    Left = 32
+    Top = 16
+  end
+  object driver_mysql: TFDPhysMySQLDriverLink
+    Left = 32
+    Top = 64
+  end
+  object cursor_ccs: TFDGUIxWaitCursor
+    Provider = 'Forms'
+    Left = 32
+    Top = 112
+  end
+  object qry_user: TFDQuery
+    Connection = conn_ccs
+    Left = 32
+    Top = 160
+  end
 end
--- ufrm_dm.pas
+++ ufrm_dm.pas
@@ -3,15 +3,37 @@ unit ufrm_dm;
 interface
 
 uses
-  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
-  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
-  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
-  FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS,
-  FireDAC.DApt.Intf, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client, FireDAC.Comp.UI;
+  System.SysUtils,
+  System.Classes,
+
+  Data.DB,
+
+  FireDAC.Stan.Intf,
+  FireDAC.Stan.Option,
+  FireDAC.Stan.Error,
+  FireDAC.UI.Intf,
+  FireDAC.Phys.Intf,
+  FireDAC.Stan.Def,
+  FireDAC.Stan.Pool,
+  FireDAC.Stan.Async,
+  FireDAC.Phys,
+  FireDAC.Phys.MySQL,
+  FireDAC.Phys.MySQLDef,
+  FireDAC.VCLUI.Wait,
+  FireDAC.Stan.Param,
+  FireDAC.DatS,
+  FireDAC.DApt.Intf,
+  FireDAC.DApt,
+  FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client,
+  FireDAC.Comp.UI;
 
 type
-  Tdm = class(TDataModule)
+  Tfrm_dm = class(TDataModule)
+    conn_ccs: TFDConnection;
+    driver_mysql: TFDPhysMySQLDriverLink;
+    cursor_ccs: TFDGUIxWaitCursor;
+    qry_user: TFDQuery;
   private
     { Private declarations }
   public
@@ -19,13 +41,11 @@ type
   end;
 
 var
-  dm: Tdm;
+  frm_dm: Tfrm_dm;
 
 implementation
 
 {%CLASSGROUP 'Vcl.Controls.TControl'}
-
-uses UserSessionUnit;
 
 {$R *.dfm}
 
